3d161a26e0c478bf18b2361360342b19128dfdae
noli-noli <103591297+noli-noli@users.noreply.github.com>
2024-11-21 21:27:27 +0900

    Update service-install.bat

--- service-install.bat
+++ service-install.bat
@@ -1,12 +1,5 @@
 @echo off 
 net session >nul 2>&1
-
-rem "コードを覗くなんて..あなたはエッチですね。"
-rem "どうせならコードの説明をしましょうか"
-rem ""管理者で実行していない場合プログラムを終了"
-rem "管理者で実行している場合相対パスにある"Tor"ディレクトリ配下のファイル群を"C:\Program Files\Tor-system"にコピーを行う"
-rem "コピー後、対象のディレクトリにアクセスしtorサービスをインストール"
-rem "っていうのを実行していますね。by.NORINORI"
  
 if %ERRORLEVEL% equ 0 (
   goto install
